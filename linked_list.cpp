01b0d2c9789cddc8c2c7f2bc8bedcec9c1c88679
Yash Raj <yash524767@gmail.com>
2024-03-10 15:55:37 +0530

    DOne

--- linked_list.cpp
+++ linked_list.cpp
@@ -1,43 +1,89 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-//creation of node
-class Node{
-    public:
+class Node
+{
+public:
     int data;
-    Node* next;
+    Node *next;
 
-    //constructor to inistalization
-    Node(int data){
-        this->data = data;
-        this->next = NULL;
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
     }
 };
 
+void insertAtHead(Node *&head, int val)
+{
+    Node *newNode = new Node(val);
+    newNode->next = head;
+    head = newNode;
+}
 
- void InsertAtbegning(Node* &head, int d){
-    Node* temp = new Node(d);
-    temp-> next = head;
-    head = temp;
- }
-
- void print(Node* &head){
-    Node* temp = head;
-    while (temp != NULL)
+void insertAtEnd(Node *&head, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL)
     {
-        cout<<temp->data<<endl;
+        head = newNode;
+        return;
+    }
+    Node *temp = head;
+    while (temp->next != NULL)
+    {
         temp = temp->next;
     }
-    
- }
+    temp->next = newNode;
+}
+
+void display(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
 
 
-int main(){
-    Node* node1 = new Node(101);
-    Node* node2 = new Node(102);
-    cout<<node1->data<<endl;
-    cout<<node2->data<<endl;
-    cout<<node2->next;
+int main()
+{
+    Node *head = NULL;
+    int ch, data;
+    while (true)
+    {
+        cout << "\n";
+        cout << "1. Insert at First" << endl;
+        cout << "2. Insert at Last" << endl;
+        cout << "3. Display" << endl;
+        cout << "4. Exit" << endl;
+        cout << "Enter your choice- ";
+        cin >> ch;
+        switch (ch)
+        {
+        case 1:
+            cout << "Enter the value- ";
+            cin >> data;
+            insertAtHead(head, data);
+            break;
+        case 2:
+            cout << "Enter the value- ";
+            cin >> data;
+            insertAtEnd(head, data);
+            break;
+        case 3:
+            display(head);
+            break;
+        case 4:
+            exit(0);
+        default:
+            cout << "Invalid choice" << endl;
+            break;
+        }
+    }
+
     return 0;
-
 }
